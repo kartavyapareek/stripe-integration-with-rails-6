7f966b3dc55e1928b458a9bd275056b78da5bddc
kartavyapareek <pareekkartavya@gmail.com>
2020-03-19 00:58:17 +0530

    home page view created and minor UI changes

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative 'boot'
 
 require 'rails/all'
@@ -10,6 +12,8 @@ module StripeIntegrationInSubscriptionApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+
+    config.assets.paths << Rails.root.join('app', 'assets', 'img')
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
