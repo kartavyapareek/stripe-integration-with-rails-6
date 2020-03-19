7c12d7e6a92bc148d0ed734a4525b53b6d15f656
kartavyapareek <pareekkartavya@gmail.com>
2020-03-19 11:14:46 +0530

    UI work complete for select package

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,9 +1,17 @@
 # frozen_string_literal: true
 
+# subscription controller
 class SubscriptionsController < ApplicationController
   def new
     @subscription = Subscription.new
+    @subscription_type = SubscriptionCategory.find params[:subscription_type]
   end
 
-  def create; end
+  def create
+    debugger
+  end
+
+  def subscription_type_list
+    @subscription_types = SubscriptionCategory.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :subscriptions
+  resources :subscriptions do
+    collection do
+      get 'subscription_type_list', to: 'subscriptions#subscription_type_list'
+    end
+  end
 
   root 'welcome#index'
 end
